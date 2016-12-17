2bdca3469e830fdff3e1837839f2dd08b05ab60e
lilpablo <jderouen@lion.lmu.edu>
2016-12-16 19:56:25 -0800

    prints out the characters still

--- Trie.java
+++ Trie.java
@@ -1,84 +1,66 @@
 import java.util.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class Trie{
-	private String [] Words = new String[10];
-	private int[] counts = new int[10];
-    public class Node{
-		char c;
-		Node[] children;
-		boolean word;			// end of node set true when the list makes a word
-		int wordCount;
-	
-		public Node(){
-			this.c= ' ';
-			this.children = new Node[26];
-			this.word = false;
-			this.wordCount = 0;
-		}
-	}
-	
-	
-	
-	Node root;
-	Node[] children; 
-	
+	private String [] topWords = new String[10];
+	private int [] counts = new int[10];
+	char c;
+	Trie[] children;
+	boolean word;			// end of node set true when the list makes a word
+	int wordCount;
 	
 	public Trie(){
-		root = null;
-		this.children = new Node[26];
-		
+		this.c = 0;
+		this.children = new Trie[26];
+		this.word = false;
+		this.wordCount = 0;
 	}
-	public String normalize(){
-		String temp= "";
-		for (int i = 0; i< this.length(); i++;){
-			// Get rid of all but letters 64 to 91 or 96 to 123 :) and keeps the space
-			char n = this.charAt(i);
-			if (n== ' ')
-				temp+= n;
-			if ((n>='A'&& n<= 'Z')){
-				temp+= Character.toLowerCase(this.charAt(i));
-			} else if(n> 96 && n < 123){
-				temp+=this.charAt(i);
-			} 
+
+	
+	public String normalize(String s){
+		String temp = "";
+		for( int i = 0; i < s.length(); i++){
+			char n = s.charAt(i);
+			if (n >= 'A' && n <= 'Z'){
+				temp += Character.toLowerCase(n);
+
+			} else if(n >= 'a' && n<= 'z'){
+				temp+=n;
+			}
 		}
+		System.out.println(temp);
+		System.out.println(s);
 		return temp;
 	}
-    public int returnEnd(){
-        this.word = true;
-		this.wordCount++;
-        return this.wordCount;
-        
-    }
-	public void compareValues(String s){
-		if(s.charAt(0) == null){
-			if(Words[0] == ""){
-				Words[1] = s;
-				counts[1] = this.wordCount;
-			}else {
-				for (int i = 0; i < 10; i++){
-					
-				}
-			}
-			
-		}
-	}
 	public void add(String s){
-		int l = 0;
-		
-		if(s.charAt(0) == null){
-			int l = s.returnEnd();
+
+	String temp = normalize(s);
+	adder(temp);
+	}
+	public void adder(String s){
+
+		if(s.length() ==0){
+			this.word = true;
+			this.wordCount++;
+			if(topWords[0]== ""){
+				topWords[0] = s;
+				counts[0] = this.wordCount;
+				System.out.println(topWords[0] + " "+ counts[0]);
+			} 
+			//
+
 			return;
 		}
-		s = s.normalize();
+
 		char letter = s.charAt(0);
-		int index = letter - 'a';  // 'a' value of 97 , b = 98 so b is index 1 
-		
+		int index = (int)letter - 97;  // 'a' value of 97 , b = 98 so b is index 1 
+		System.out.println(index);
 		if (this.children[index]== null){
 			this.children[index] = new Trie();
 		}
-		
-		this.children[index.add(s.substring(1))];//recurrisivly adds letters by shorting the string 
+		s = s.substring(1);
+		this.children[index].add(s);//recurrisivly adds letters by shoriting the string 
 	}
 	
 	public boolean isWord(String s){
@@ -94,20 +76,7 @@ public class Trie{
 		return this.children[index].isWord(s.substring(1));
 	}
 
-	public void wordSearch(Node node){
-        if (this.word ){
-            for (int i = 0; i < 13; i++){
-                wordSearch(this.children[i]);
-            }
-            visit(Node);
-            for (int i = 13 ; i < 26; i++){
-                wordSearch(this.chilren[i]);
-            }
-            
-            }
-        }
-    }
-	    public static void main(String[] args) throws IOException {
+		    public static void main(String[] args) throws IOException {
         Trie trie = new Trie();
         Scanner s = null;
         try {
@@ -115,8 +84,9 @@ public class Trie{
             while (s.hasNext()) {
                 String item = s.next(); // Scanner splits input on whitespace, by default
                 trie.add(item);
-                System.out.println(add);
+                System.out.println(trie.c);
             }
+
         } finally {
             if (s != null) {
                 s.close();
@@ -125,3 +95,4 @@ public class Trie{
 
         
     }
+}
