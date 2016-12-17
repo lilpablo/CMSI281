476b5a1dd2736dae94aff48ed1b864a63d3e7d1e
lilpablo <jderouen@lion.lmu.edu>
2016-12-16 22:33:19 -0800

    Updated trie
    So far prints out each character in the node and each word with count of each time word is called

--- Trie.java
+++ Trie.java
@@ -3,18 +3,23 @@ import java.io.*;
 import java.util.Scanner;
 
 public class Trie{
+	public static String worder = " ";
 	private String [] topWords = new String[10];
 	private int [] counts = new int[10];
 	char c;
 	Trie[] children;
 	boolean word;			// end of node set true when the list makes a word
 	int wordCount;
+	String fullWord;
+	int layer;
 	
 	public Trie(){
 		this.c = 0;
 		this.children = new Trie[26];
 		this.word = false;
 		this.wordCount = 0;
+		this.fullWord = " ";
+		this.layer = 1;
 	}
 
 	
@@ -22,32 +27,43 @@ public class Trie{
 		String temp = "";
 		for( int i = 0; i < s.length(); i++){
 			char n = s.charAt(i);
+			if(n == ' '){
+				temp += n;
+			}
 			if (n >= 'A' && n <= 'Z'){
 				temp += Character.toLowerCase(n);
 
 			} else if(n >= 'a' && n<= 'z'){
 				temp+=n;
-			}
+			} 
+
 		}
-		System.out.println(temp);
-		System.out.println(s);
+		
 		return temp;
 	}
 	public void add(String s){
 
 	String temp = normalize(s);
 	adder(temp);
+	if(this.word){
+			System.out.println(s + " , " + this.wordCount);
+			N = 0;
+		}
 	}
-	public void adder(String s){
 
+	public static int N = 0;
+
+	public void adder(String s){
 		if(s.length() ==0){
 			this.word = true;
 			this.wordCount++;
-			if(topWords[0]== ""){
-				topWords[0] = s;
-				counts[0] = this.wordCount;
-				System.out.println(topWords[0] + " "+ counts[0]);
-			} 
+			N = this.wordCount;
+			// this.fullWord = worder;
+			// if(topWords[0]== ""){
+			// 	topWords[0] = s;
+			// 	counts[0] = this.wordCount;
+			//  System.out.println(topWords[0] + " "+ counts[0]);
+			
 			//
 
 			return;
@@ -55,28 +71,64 @@ public class Trie{
 
 		char letter = s.charAt(0);
 		int index = (int)letter - 97;  // 'a' value of 97 , b = 98 so b is index 1 
-		System.out.println(index);
 		if (this.children[index]== null){
 			this.children[index] = new Trie();
+			this.children[index].layer = this.layer + 1;
+			this.children[index].c = letter;
+			this.children[index].fullWord = this.fullWord + s.charAt(0);
 		}
-		s = s.substring(1);
-		this.children[index].add(s);//recurrisivly adds letters by shoriting the string 
+		//s = s.substring(1);
+		this.children[index].add(s.substring(1));//recurrisivly adds letters by shoriting the string 
+	}
+
+	static String temporary = "";
+	static int intTemp = 0;
+	public static void toString(Trie trie){
+
+		if(trie != null){
+			visit(trie);
+			if(trie.word){
+				temporary = "";
+			}
+
+			for(int i = 0; i < 26; i++){
+				if(trie.children[i]!= null){
+					toString(trie.children[i]);
+				}
+			}
+		}
+
+	}
+
+	public static void visit(Trie trie){
+		System.out.println(trie.c);
+		if(trie.word){
+			temporary = trie.fullWord;
+			System.out.println(trie.wordCount + " , "+ temporary);
+			trie.word = false;
+
+			//temporary.substring(0, temporary.length()-1);
+			//System.out.println(trie.fullWord);
+		}
+		//System.out.println(trie.c);
 	}
 	
-	public boolean isWord(String s){
-		if(s.isEmpty()){
-			return this.word;
-		}
+	// public boolean isWord(String s){
+	// 	if(s.isEmpty()){
+	// 		return this.word;
+	// 	}
 
-		int index = s.charAt(0) - 'a';
+	// 	int index = s.charAt(0) - 'a';
 		
-		if(this.children[index] == null){
-			return false;
-		}
-		return this.children[index].isWord(s.substring(1));
-	}
+	// 	if(this.children[index] == null){
+	// 		return false;
+	// 	}
+	// 	return this.children[index].isWord(s.substring(1));
+	// }
 
-		    public static void main(String[] args) throws IOException {
+
+
+	public static void main(String[] args) throws IOException {
         Trie trie = new Trie();
         Scanner s = null;
         try {
@@ -84,7 +136,6 @@ public class Trie{
             while (s.hasNext()) {
                 String item = s.next(); // Scanner splits input on whitespace, by default
                 trie.add(item);
-                System.out.println(trie.c);
             }
 
         } finally {
@@ -92,6 +143,7 @@ public class Trie{
                 s.close();
             }
         }
+        toString(trie);
 
         
     }
